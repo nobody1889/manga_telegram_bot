ec4cb1948de3c2f039048288d9d246ac7db3ac26
nobody <nobodyisherefornow1977@gamil.com>
2024-07-03 17:10:43 -0400

    -adding one new exception for internet problem -rename those functions which not exist -add html response

--- telegram_bot_properties/updates.py
+++ telegram_bot_properties/updates.py
@@ -1,27 +1,28 @@
+import httpx
 from telegram import Update
 from telegram.ext import ContextTypes
 
 from stuff import search_new
 
 
-async def reload_check(name: str):
-    return await search_new(name)
-
-
 async def check_comics_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         await update.message.reply_text('search for new updates')
-        comics = await reload_check(str(update.message.from_user.id))
+        comics = await search_new(str(update.message.from_user.id))
         if comics:
             for comic in comics:
-                text0 = f"new chapter for {comic} :\n"
+                text0 = f"new chapters for {comic} :\n"
                 text1 = [one for one in comics[comic]]
                 text = text0 + '\n'.join(text1)
-                await update.message.reply_text(text)
+                await update.message.reply_text(text, parse_mode='Html')
         else:
             await update.message.reply_text('no new chapter')
+
     except ValueError:
         await update.message.reply_text('no comics added please add one via \n</start>')
 
+    except httpx.ConnectTimeout:
+        await update.message.reply_text('please try again')
 
-__all__ = ('check_comics_command', 'reload_check')
+
+__all__ = ('check_comics_command',)
