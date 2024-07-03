9a42fac5e5c51cea965107cc6d2b6bbe43d429dc
nobody <nobodyisherefornow1977@gamil.com>
2024-07-03 17:11:04 -0400

    add staticmethod

--- clients/commons/request.py
+++ clients/commons/request.py
@@ -4,11 +4,13 @@ import httpx
 
 
 class Requests:
-    def sync_req(self, url: str):
+    @staticmethod
+    def sync_req(url: str):
         res = requests.get(url)
         return res
 
-    async def __sub_async_req(self, url: str, client):
+    @staticmethod
+    async def __sub_async_req(url: str, client):
         return client.get(url)
 
     async def aget(self, urls: list[str]) -> list:
