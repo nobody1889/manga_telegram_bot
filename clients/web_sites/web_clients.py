210c341e3f5e6dc89284a33890d59c1358d69967
nobody <nobodyisherefornow1977@gamil.com>
2024-07-04 17:54:06 -0400

    -some new parts to full the json file better

--- clients/web_sites/web_clients.py
+++ clients/web_sites/web_clients.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 
 
 class manhwax:
+    url = 'https://manhwax.org'
     @staticmethod
     def comic_main_page(soup: BeautifulSoup, data: dict) -> dict:
         chapters = soup.find('div', {"class": "eplister", "id": "chapterlist"}).find('ul').find_all('li')
@@ -12,14 +13,14 @@ class manhwax:
 
         data["name"] = soup.find('h1', {"class": "entry-title", "itemprop": "name"}).text
         data["rate"] = float(soup.find('div', {"class": "num", "itemprop": "ratingValue"}).text)
+        data["status"] = soup.find("div", class_="tsinfo").find("i").text
+        all_tags = soup.find("span", class_="mgen").find_all('a')
+        data["genres"] = [tag.text for tag in all_tags]
         data["cover_url"] = soup.find('img', {'class': 'attachment- size- wp-post-image', 'alt': data["name"]})['src']
 
         return data
 
-    @staticmethod
-    def new_comics(soup: BeautifulSoup):
-        url = 'https://manhwax.org'  # first page
-
+    def new_comics(self, soup: BeautifulSoup):
         comics = [page.find('a')["href"] for page in soup.find_all("div", class_="bs styletere")]
         next_page = soup.find("div", class_="hpage").find_all('a')[-1]['href']
 
@@ -39,6 +40,8 @@ class mangahentai:
 
         data["name"] = soup.find('h1').text.split('\n')[-1]
         data["rate"] = float(soup.find("span", class_="score font-meta total_votes").text) * 2
+        # all_tags = soup.find("span", class_="mgen").find_all('a')
+        # data["genres"] = [tag.text for tag in all_tags]
         data["cover_url"] = soup.find('img', class_="img-responsive")['src']
 
         return data
@@ -63,6 +66,10 @@ class chapmanganato:
 
         data["name"] = soup.find('h1').text.split('\n')[-1]
         data["rate"] = float(soup.find('em', {'property': "v:average"}).text) * 2
+
+        all_tags = soup.find("table", class_="variations-tableInfo").find_all("td", class_="table-value")
+        data["status"] = all_tags[-2].text
+        data["genres"] = [tag.text for tag in all_tags[-1] if tag.text not in [' - ', '\n']]
         data["cover_url"] = soup.find("img", class_="img-loading")['src']
 
         return data
@@ -90,6 +97,9 @@ class comixextra:
         new_name = [n for n in name[1].split(' ') if n]
         data["name"] = ' '.join(new_name)
         data["rate"] = None
+        all_tags = soup.find("div", class_="movie-meta-info").find('dl').find_all('a')
+        data['status'] = all_tags[0]
+        data["genres"] = [tag.text for tag in all_tags[1:]]
         data["cover_url"] = soup.find("img", {'alt': data['name']})['src']
 
         return data
