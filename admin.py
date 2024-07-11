d000b8ce17b10af15e093b3598f16f18be2a00b1
nobody <148004508+nobody1889@users.noreply.github.com>
2024-07-11 06:10:33 -0400

    Create admin.py
    create admin file to handle admin orders

---
+++ admin.py
@@ -0,0 +1,15 @@
+ADMIN = ""
+Channel = ""
+
+
+async def admin_handler(text, update, context):
+    text = text.lower()
+    match text:
+        case "kill":
+            pass
+        case "pause":
+            pass
+        case "save":
+            pass
+        case "users":
+            pass
